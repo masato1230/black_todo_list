2a89d2d6414176e9d7932847eff7cdd9f446be0a
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-05-10 22:31:13 +0900

    Create Todo and Task Constants

--- app/src/main/java/com/jp_funda/blacktodolist/Constants/TaskConstants.java
+++ app/src/main/java/com/jp_funda/blacktodolist/Constants/TaskConstants.java
@@ -1,13 +1,13 @@
 package com.jp_funda.blacktodolist.Constants;
 
-public class TodoConstants {
+public class TaskConstants {
     public static final int DB_VERSION = 1;
-    public static final String DB_NAME = "todoDB";
-    public static final String TABLE_NAME = "todoTable";
+    public static final String DB_NAME = "taskDB";
+    public static final String TABLE_NAME = "taskTable";
     // table columns
     public static final String KEY_ID = "id";
     public static final String KEY_TITLE = "title";
     public static final String KEY_MEMO = "memo";
     public static final String KEY_REMIND = "remind";
-    public static final String KEY_TASK_IDS = "taskIds";
+    public static final String KEY_TODO_ID = "todo_id";
 }
